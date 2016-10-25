4e995b9a112ab84f5cdbcbf36ea8efd9d90fd99f
KennyEmbrechts <kenny.embrechts@gmail.com>
2016-10-25 14:20:06 +0200

    Start of project

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.IOException;
+
+/**
+ * Created by Kenny on 25/10/16.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+    }
+}
