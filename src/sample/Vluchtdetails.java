31dedd0e29bd7c571e0630a48f9f26ab010de572
Jente <jente.adams@gmail.com>
2016-10-28 11:26:38 +0200

    fixed more import errors

--- src/sample/Vluchtdetails.java
+++ src/sample/Vluchtdetails.java
@@ -1,17 +1,14 @@
 package sample;
 
-
-
-
 public class Vluchtdetails
 {
-	public Locatie Herkomst;
-	public Locatie Besteming;
-	public int VluchtNr;
-	public public int uurVertrek;
-	public public int uurAankomst;
-	public private String Piloot;
-	public private int VliegtuigNr;
+	//public Locatie Herkomst;
+	//public Locatie Besteming;
+	public  int VluchtNr;
+	public int uurVertrek;
+	public int uurAankomst;
+	private String Piloot;
+	private int VliegtuigNr;
 	public void ToonDetails()
 	{
 	}
