40e4c84794f1819ecdd86de3204749b77ed5bfa9
Jente <jente.adams@gmail.com>
2016-11-16 14:23:35 +0100

    fixed merge confliicts

--- src/luchthavenbeheer/Main.java
+++ src/luchthavenbeheer/Main.java
@@ -1,28 +1,3 @@
-<<<<<<< HEAD:src/sample/Main.java
-package sample;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
-=======
 package luchthavenbeheer;
 
 import javafx.application.Application;
@@ -46,4 +21,3 @@ public class Main extends Application {
         launch(args);
     }
 }
->>>>>>> d5adf5e8d594d6b93a3009f7e4a9ebc0b1cb2812:src/luchthavenbeheer/Main.java
